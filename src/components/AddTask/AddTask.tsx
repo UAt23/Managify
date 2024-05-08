2a764549307e14887546e51771521d1c0ab228d4
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2024-05-08 14:50:30 +0300

    Drop menu selection styles

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -1,18 +1,30 @@
 import { Fragment, useState } from "react";
 
 import Button from "@mui/material/Button";
-import { alpha, FormControl, InputBase, InputLabel, styled } from "@mui/material";
+import {
+	alpha,
+	FormControl,
+	InputBase,
+	InputLabel,
+	MenuItem,
+	Select,
+	SelectChangeEvent,
+	styled,
+	Theme,
+	useTheme,
+} from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-// import Typography from "@mui/material/Typography";
 import AddRounded from "@mui/icons-material/AddRounded";
 
 import styles from "./AddTask.module.scss";
 import { Tick } from "../icons";
+import { boards } from "../../constants/boards";
+import { BoardColumn } from "../../models/boards.model";
 
 const BootstrapDialog = styled(Dialog)(() => ({
 	"& .MuiDialog-paper": {
@@ -22,9 +34,9 @@ const BootstrapDialog = styled(Dialog)(() => ({
 		borderRadius: "0.75rem",
 	},
 	"& .MuiDialogContent-root": {
-		display: 'flex',
-		flexDirection: 'column',
-		rowGap: '1.5rem'
+		display: "flex",
+		flexDirection: "column",
+		rowGap: "1.5rem",
 	},
 	"& .MuiDialogActions-root": {
 		padding: "1.5rem",
@@ -36,8 +48,8 @@ const InputField = styled(InputBase)(({ theme }) => ({
 		marginTop: theme.spacing(3),
 	},
 	"& .MuiInputBase-input": {
-		color: '#FEF7EE',
-		borderRadius: '.75rem',
+		color: "#FEF7EE",
+		borderRadius: ".75rem",
 		position: "relative",
 		backgroundColor: "transparent",
 		border: "2px solid #3A3E44",
@@ -51,17 +63,66 @@ const InputField = styled(InputBase)(({ theme }) => ({
 			"box-shadow",
 		]),
 		"&:focus": {
-			boxShadow: `${alpha(
-				'#7E878D',
-				0.25
-			)} 0 0 0 0.2rem`,
-			borderColor: '#7E878D',
+			borderRadius: ".75rem",
+			boxShadow: `${alpha("#7E878D", 0.25)} 0 0 0 0.2rem`,
+			borderColor: "#7E878D",
 		},
 	},
 }));
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250,
+			padding: '.5rem',
+			borderRadius: '.75rem',
+			border: '1px solid #3A3E44',
+			background: '#191B1F',
+			color: '#FEF7EE'
+		},
+	},
+};
+
+const names = [
+	'Oliver Hansen',
+	'Van Henry',
+	'April Tucker',
+	'Ralph Hubbard',
+	'Omar Alexander',
+	'Carlos Abbott',
+	'Miriam Wagner',
+	'Bradley Wilkerson',
+	'Virginia Andrews',
+	'Kelly Snyder',
+];
+
+function getStyles(name: string, boardName: string[], theme: Theme) {
+	return {
+		fontWeight:
+			boardName.indexOf(name) === -1
+				? theme.typography.fontWeightRegular
+				: theme.typography.fontWeightMedium,
+	};
+}
+
 export default function AddTask() {
+	const theme = useTheme();
 	const [open, setOpen] = useState(false);
+	const [boardName, setBoardName] = useState<string[]>([]);
+
+	const boardOptions: Array<BoardColumn> = Object.values(boards).map((item) => item)
+
+	const handleChange = (event: SelectChangeEvent<typeof boardName>) => {
+		const {
+			target: { value },
+		} = event;
+		setBoardName(
+			typeof value === "string" ? value.split(",") : value
+		);
+	};
 
 	const handleClickOpen = () => {
 		setOpen(true);
@@ -150,18 +211,111 @@ export default function AddTask() {
 					<FormControl
 						variant="standard"
 						sx={{
-							width: '100%'
+							width: "100%",
 						}}
 					>
-						<InputLabel  
+						<InputLabel
 							htmlFor="task-name-input"
 							sx={{
-								color: '#7E878D !important',
+								color: "#7E878D !important",
 							}}
 						>
 							Task name
 						</InputLabel>
-						<InputField defaultValue="New tasks awaits..." id="task-name-input" />
+						<InputField
+							defaultValue="New tasks awaits..."
+							id="task-name-input"
+						/>
+					</FormControl>
+					<FormControl
+						variant="standard"
+						sx={{
+							width: "100%",
+						}}
+					>
+						<InputLabel
+							disableAnimation={true}
+							shrink={false}
+							htmlFor="status-input"
+							sx={{
+								color: "#7E878D !important",
+								position: 'initial',
+								fontFamily: 'inherit',
+								fontSize: '0.75rem'
+							}}
+						>
+							Status
+						</InputLabel>
+						<Select
+							id="status-input"
+							IconComponent={() => <AddRounded sx={{display: 'none'}} />}
+							value={boardName}
+							onChange={handleChange}
+							input={<InputField />}
+							labelId="status-input"
+							MenuProps={MenuProps}
+						>
+							{boardOptions.map((option) => (
+								<MenuItem
+									key={option.title}
+									value={option.title}
+									style={getStyles(option.title, boardName, theme)}
+									sx={{
+										borderRadius: '.25rem',
+										marginBottom: '.25rem',
+									}}
+								>
+									<span className={styles.colorBall} style={{backgroundColor: `${option.color}`}}></span>
+									{option.title}
+								</MenuItem>
+
+							))}
+						</Select>
+					</FormControl>
+					<FormControl
+						variant="standard"
+						sx={{
+							width: "100%",
+						}}
+					>
+						<InputLabel
+							disableAnimation={true}
+							shrink={false}
+							htmlFor="tags-input"
+							sx={{
+								color: "#7E878D !important",
+								position: 'initial',
+								fontFamily: 'inherit',
+								fontSize: '0.75rem'
+							}}
+						>
+							Tags
+						</InputLabel>
+						<Select
+							id="tags-input"
+							IconComponent={() => <AddRounded sx={{display: 'none'}} />}
+							multiple
+							value={boardName}
+							onChange={handleChange}
+							input={<InputField />}
+							labelId="status-input"
+							MenuProps={MenuProps}
+						>
+							{names.map((name) => (
+								<MenuItem
+									key={name}
+									value={name}
+									style={getStyles(name, boardName, theme)}
+									sx={{
+										borderRadius: '.25rem',
+										background: 'red',
+										marginBottom: '.25rem',
+									}}
+								>
+									{name}
+								</MenuItem>
+							))}
+						</Select>
 					</FormControl>
 				</DialogContent>
 				<DialogActions
